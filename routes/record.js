5fea3e8ac037157349b0ce27951cc371501fe579
YousefAllam221B <yousef.allam221b@gmail.com>
2022-02-05 16:46:10 +0200

    add, update and delete notes works without sessions

--- routes/record.js
+++ routes/record.js
@@ -11,39 +11,67 @@ const dbo = require("../db/conn");
 // This help convert the id from string to ObjectId for the _id.
 const ObjectId = require("mongodb").ObjectId;
 
-// This section will help you get a single record by id
+// Login route
 recordRoutes.route("/login/:name/:password").get(function (req, res) {
   let db_connect = dbo.getDb();
   let myquery = { name: (req.params.name.substring(1)), password: (req.params.password.substring(1))};
 
   db_connect
-      .collection("Users")
       .findOne(myquery, function (err, result) {
         if (err) throw err;
         res.json(result);   
       });
 });
 
-// This section will help you create a new record.
+// Add Note Route
+recordRoutes.route("/addNote/:title").post(function (req, response) {
+  let db_connect = dbo.getDb();
+  let myquery = { name : "yousef"};
+  db_connect
+    .updateOne(myquery, {$push:{'Notes': {"title" : req.params.title.substring(1), "value": ''}}}, function (err, res) {
+      if (err) throw err;
+      response.json(res);
+  });
+});
+
+// Update Note Text Route
 recordRoutes.route("/update/:index/:updatedText").post(function (req, response) {
   let db_connect = dbo.getDb();
   let myquery = { name : "yousef"};
 
   let newvalues = {}
-  newvalues["Notes." + parseInt(req.params.index.substring(1))] = {"value": req.params.updatedText.substring(1)}
+  newvalues["Notes." + parseInt(req.params.index.substring(1)) + ".value"] = req.params.updatedText.substring(1)
   console.log(newvalues);
-  // db_connect
-  //   .collection("Users")
-  //   .updateOne(myquery, {$set:{newvalues}}, function (err, res) {
-  //     if (err) throw err;
-  //     console.log("1 document updated");
-  //     console.log(res);
-  //     response.json(res);
-    
-  // });
+  db_connect
+    .updateOne(myquery, {$set:newvalues}, function (err, res) {
+      if (err) throw err;
+      response.json(res);
+  });
 });
 
+//Delete Note Route
+recordRoutes.route("/deleteNote/:index").post(function (req, response) {
+  deletingNote(req.params.index);
+});
 
+//async function to delete notes. Async to wait for setting the value with null then remove any null value.
+async function deletingNote(noteIndex)
+{
+  let db_connect = dbo.getDb();
+  let myquery = { name : "yousef"};
+  let newvalues = {}
+  newvalues["Notes." + noteIndex] = 1
+  console.log(newvalues);
+  await db_connect
+    .updateOne(myquery, {$unset: newvalues }, function (err, res) {
+  });
+  db_connect
+  .updateOne(myquery, {$pull: {"Notes" : null} }, function (err, res) {
+    if (err) throw err;
+  });
+ }
+
+{
 // This section will help you update a record by id.
 // recordRoutes.route("/record/add").post(function (req, response) {
 //   let db_connect = dbo.getDb();
@@ -79,5 +107,5 @@ recordRoutes.route("/update/:index/:updatedText").post(function (req, response) 
 //     response.status(obj);
 //   });
 // });
-
+}
 module.exports = recordRoutes;
