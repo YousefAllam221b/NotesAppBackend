7065f2ec5ffae075a09f94a08fbce33c4f55235d
YousefAllam221B <yousef.allam221b@gmail.com>
2022-02-08 16:03:02 +0200

    Working where every note has its own objectID

--- routes/record.js
+++ routes/record.js
@@ -11,97 +11,7 @@ const dbo = require("../db/conn");
 // This help convert the id from string to ObjectId for the _id.
 const ObjectId = require("mongodb").ObjectId;
 
-// Login route
-recordRoutes.route("/login/:name/:password").get(async function (req, res) {
-  let db_connect = dbo.getDb();
-  let myquery = { name: (req.params.name), password: (req.params.password)};
-
-  db_connect
-      .findOne(myquery, function (err, result) {
-        if (err) throw err;
-        res.json(result);   
-      });
-});
-
-// Add Note Route
-recordRoutes.route("/addNote/:userID/:title").post(function (req, response) {
-  let db_connect = dbo.getDb();
-  let myquery = { _id: new ObjectId(req.params.userID)};
-  db_connect
-    .updateOne(myquery, {$push:{'Notes': {"title" : req.params.title, "value": ''}}}, function (err, res) {
-      if (err) throw err;
-      response.json(res);
-  });
-});
-
-recordRoutes.route("/addNote/:userID/:title/:value").post(function (req, response) {
-  let db_connect = dbo.getDb();
-  let myquery = { _id: new ObjectId(req.params.userID)};
-  db_connect
-    .updateOne(myquery, {$push:{'Notes': {"title" : req.params.title, "value": req.params.value}}}, function (err, res) {
-      if (err) throw err;
-      response.json(res);
-  });
-});
-
-// Update Note title Route
-recordRoutes.route(["/updateTitle/:userID/:index/:updatedTitle","/updateTitle/:userID/:index/"]).post(function (req, response) {
-  let db_connect = dbo.getDb();
-  let myquery = { _id: new ObjectId(req.params.userID)};
-
-  let newvalues = {}
-  newvalues["Notes." + parseInt(req.params.index) + ".title"] = req.params.updatedTitle
-  db_connect
-    .updateOne(myquery, {$set:newvalues}, function (err, res) {
-      if (err) throw err;
-      response.json(res);
-  });
-});
-
-// Update Note Text Route
-recordRoutes.route(["/update/:userID/:index/:updatedText","/update/:userID/:index/"]).post(function (req, response) {
-  let db_connect = dbo.getDb();
-  let myquery = { _id: new ObjectId(req.params.userID)};
-
-  let newvalues = {}
-  newvalues["Notes." + parseInt(req.params.index) + ".value"] = req.params.updatedText
-  db_connect
-    .updateOne(myquery, {$set:newvalues}, function (err, res) {
-      if (err) throw err;
-      response.json(res);
-  });
-});
-recordRoutes.route("/update/:userID/:index/").post(function (req, response) {
-  let db_connect = dbo.getDb();
-  let myquery = { _id: new ObjectId(req.params.userID)};
-
-  let newvalues = {}
-  newvalues["Notes." + parseInt(req.params.index) + ".value"] = req.params.updatedText
-  db_connect
-    .updateOne(myquery, {$set:newvalues}, function (err, res) {
-      if (err) throw err;
-      response.json(res);
-  });
-});
-//Delete Note Route
-recordRoutes.route("/deleteNote/:userID/:index").post(async function (req, response) {
-  let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId(req.params.userID)};
-  let newvalues = {}
-  newvalues["Notes." + req.params.index] = 1
-  await db_connect
-    .updateOne(myquery, {$unset: newvalues }, function (err, res) {
-  });
-  await db_connect
-  .updateMany(myquery, {$pull: {Notes : null} }, function (err, res) {
-    if (err) throw err;
-  });
-  await db_connect
-  .updateMany(myquery, {$pull: {Notes : null} }, function (err, res) {
-    if (err) throw err;
-  });
-});
-
+// Check if Username exists before Registering Route
 recordRoutes.route("/register/:name").get(function (req, res) {
   let db_connect = dbo.getDb();
   let myquery = { name: (req.params.name)};
@@ -113,6 +23,7 @@ recordRoutes.route("/register/:name").get(function (req, res) {
   });
 });
 
+// Register Route
 recordRoutes.route("/register/:name/:password").post(function (req, res) {
   let db_connect = dbo.getDb();
   let myquery = { name: (req.params.name), password: (req.params.password), Notes: []};
@@ -121,6 +32,74 @@ recordRoutes.route("/register/:name/:password").post(function (req, res) {
   .insertOne(myquery, function (err, result) {
     if (err) throw err;
     res.json(result);   
+  });
+});
+
+// Login route
+recordRoutes.route("/login/:name/:password").get(async function (req, res) {
+  let db_connect = dbo.getDb();
+  let myquery = {name: (req.params.name), password: (req.params.password)};
+  db_connect
+      .findOne(myquery, function (err, result) {
+        if (err) throw err;
+        res.json(result);   
+      });
+});
+
+// Add Note Route
+recordRoutes.route("/addNote/:userID/:title").post(async(req, response) => {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: new ObjectId(req.params.userID)};
+  let oid = new ObjectId();
+  await db_connect
+      .updateOne(myquery, {$push:{'Notes': {_id: oid, title : req.params.title, value: ''}}}, function (err, res) {
+        if (err) throw err;
+        response.json(oid);
+  });
+});
+
+// Add Notes before Login Route
+recordRoutes.route("/addNote/:userID/:title/:value").post(async (req, response) => {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: new ObjectId(req.params.userID)};
+  let oid = new ObjectId();
+  db_connect
+    .updateOne(myquery, {$push:{Notes: {_id: oid, title: req.params.title, value: req.params.value}}}, function (err, res) {
+      if (err) throw err;
+      response.json(oid);
+  });
+});
+
+// Update Note Title Route
+recordRoutes.route(["/updateTitle/:userID/:noteID/:updatedTitle","/updateTitle/:userID/:noteID/"]).post(async(req, response) => {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: new ObjectId(req.params.userID), "Notes._id": new ObjectId(req.params.noteID)};
+  db_connect
+    .updateOne(myquery, {$set:{"Notes.$.title":  req.params.updatedTitle}}, function (err, res) {
+      if (err) throw err;
+      response.json(res);
+  });
+});
+
+// Update Note Text Route
+recordRoutes.route(["/update/:userID/:noteID/:updatedText","/update/:userID/:noteID/"]).post(async(req, response) => {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: new ObjectId(req.params.userID), "Notes._id": new ObjectId(req.params.noteID)};
+  db_connect
+    .updateOne(myquery, {$set:{"Notes.$.value":  req.params.updatedText}}, function (err, res) {
+      if (err) throw err;
+      response.json(res);
+  });
+});
+
+// Delete Note Route
+recordRoutes.route("/deleteNote/:userID/:noteID").post(async (req, response) => {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.userID)};
+  await db_connect
+  .updateOne(myquery, {$pull: {Notes : {_id: new ObjectId(req.params.noteID)}} }, function (err, res) {
+    if (err) throw err;
+    response.json(res);
   });
 });
 
